4c08612987c0862ffbb4f10bf048f63a673f2d9a
LuizSptech <luiz.correia@sptech.school>
2026-07-10 00:52:59 -0300

    rota funcionando

--- src/models/feedModel.js
+++ src/models/feedModel.js
@@ -2,13 +2,13 @@ var database = require("../database/config");
 
 function scroll() {
     var instrucao = `
-    select * from POSTAGEM ORDER BY data ASC;`;
+    select * from postagem;`;
     console.log("Executando a instrução SQL:\n" + instrucao);
-    return database
+    return database.executar(instrucao)
 }
 
 function postar(titulo, descricao, imagem){
-    var instrucao = `INSERT INTO POSTAGEM (titulo, descricao, imagem) VALUES ('${titulo}', '${descricao}', '${imagem}')`;
+    var instrucao = `INSERT INTO postagem (titulo, descricao, imagem) VALUES ('${titulo}', '${descricao}', '${imagem}')`;
 
     console.log("Executando a instrução SQL:\n" + instrucao);
     return database.executar(instrucao);
