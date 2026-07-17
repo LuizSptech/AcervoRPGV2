741e4abcbdd80d25d0cae700ddc96aa8c80b937a
LuizSptech <luiz.correia@sptech.school>
2026-07-17 02:04:51 -0300

    progresso feed

--- src/models/feedModel.js
+++ src/models/feedModel.js
@@ -1,8 +1,9 @@
 var database = require("../database/config");
 
 function scroll() {
-    var instrucao = `
-    select * from postagem;`;
+    var instrucao = `SELECT usuario.nome, postagem.titulo, postagem.imagem, postagem.descricao, postagem.horario, (select COUNT(idpostagem) FROM postagem WHERE fk_usuario = 1) AS quantidade from postagem 
+JOIN usuario ON postagem.fk_usuario = usuario.id 
+order by horario DESC;`;
     console.log("Executando a instrução SQL:\n" + instrucao);
     return database.executar(instrucao)
 }
